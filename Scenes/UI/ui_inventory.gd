643ae522dbf30eaed9108d15eb65be07ca31a8c8
Christopher <savagefrankoe@gmail.com>
2023-03-09 03:45:32 -0500

    Big Update one yall been waiting for....

--- Scenes/UI/ui_inventory.gd
+++ Scenes/UI/ui_inventory.gd
@@ -29,10 +29,10 @@ var _scrolling_to_page := 1
 
 var _ui_inventory_category := preload("res://Scenes/UI/UI_Elements/ui_inventory_category.tscn")
 
-#var _category_displays_to_pages := {} 
-#var _category_displays_to_ui := {}
-#var _pages_to_category_displays := {}
-#var _current_active_ui_inventory_category : Control
+var _category_displays_to_pages := {} 
+var _category_displays_to_ui := {}
+var _pages_to_category_displays := {}
+var _current_active_ui_inventory_category : Control
 
 #
 # Item members
@@ -43,12 +43,12 @@ var _ui_inventory_category := preload("res://Scenes/UI/UI_Elements/ui_inventory_
 
 @onready var _button_left := $HBoxMain/ControlItemColumn/VBoxItemColumn/HBoxInventoryItem/ControlLeftColumn/ButtonLeft
 @onready var _button_right := $HBoxMain/ControlItemColumn/VBoxItemColumn/HBoxInventoryItem/ControlRightColumn/ButtonRight
-#@onready var _ui_item_info := $HBoxMain/ControlInfoColumn/UIItemInfo
+@onready var _ui_item_info := $HBoxMain/ControlInfoColumn/UIItemInfo
 #@onready var _context_menu_container := $UIContextMenuContainer
-#@onready var _context_menu := $UIContextMenuContainer/UIInventoryItemContextMenu
+@onready var _context_menu := $UIContextMenuContainer/UIInventoryItemContextMenu
 
 # To keep track of item highlights with mouse hover
-#var _current_ui_inventory_item_selected : Node
+var _current_ui_inventory_item_selected : Node
 
 
 func _ready():
@@ -58,16 +58,27 @@ func _ready():
 	
 func _reload() -> void:
 	_reload_items()
-
+	
+	
+func _reload_categories() -> void:
+	for child in _grid_categories.get_children():
+		_grid_categories.remove_child(child)
+		
+	for category_display in GameState.item_category_displays:
+		var ui = _ui_inventory_category.instantiate()
+		ui.set_name(category_display.display_name)
+		_grid_categories.add_child(ui)
+		
+		ui.set_category(category_display)
 func _reload_items() -> void:
 	var pages := 0
-	
+	var inventory_by_category := GameState.player_data.get_inventory_by_category()
 	for child in _item_grids_container.get_children():
 		_item_grids_container.remove_child(child)
 		#child.queue_free()
 
 	for category_display in GameState.item_category_displays:
-		var _starting_page := pages
+		var starting_page := pages
 		var category_display_amount_items := 0
 		
 		if GameState.count_inventory_items_from_category_display(category_display) > 0:
@@ -116,18 +127,16 @@ func _reload_items() -> void:
 			
 	_scroll_container.set_h_scroll(0)
 	_update_navigation()
+	
+	_reload_categories()
 
 
 func _on_button_Right_pressed() -> void:
 	_go_to_item_page(_current_scroll_page + 1)
-	
-	
 
 
 func _on_button_Left_pressed():
 	_go_to_item_page(_current_scroll_page - 1)
-	
-
 
 func _go_to_item_page(go_to_page :int) -> void:
 	if _is_scrolling:
@@ -181,6 +190,6 @@ func _update_navigation() -> void:
 	#_set_active_category_display()
 
 
-func _on_animation_player_animation_finished(_anim_name):
+func _on_animation_player_animation_finished(anim_name):
 	_current_scroll_page = _scrolling_to_page
 	_update_navigation()
