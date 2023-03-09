e69ded20075f56b0527bbb8714d99945bcd38387
Christopher <savagefrankoe@gmail.com>
2023-03-09 01:37:34 -0500

    Might have fixed bug

--- Scripts/EntityPlayer.gd
+++ Scripts/EntityPlayer.gd
@@ -2,7 +2,20 @@ extends Resource
 class_name EntityPlayer
 
 @export var inventory :Array[Resource] = []
-var inventory_by_category: set = index_inventory, get = get_inventory_by_category
+#var inventory_by_category: get = get_inventory_by_category
+var inventory_by_category: Dictionary:
+	set(new_value):
+		inventory_by_category = {}
+		for item in inventory:
+			if item is EntityItem:
+				if inventory_by_category.has(item.category_type):
+					inventory_by_category[item.category_type].append(item)
+				else:
+					inventory_by_category[item.category_type] = [item]
+				
+	get:
+		
+		return inventory_by_category
 
 
 func make_inventory_unique() -> void:
@@ -12,7 +25,7 @@ func make_inventory_unique() -> void:
 			if inventory[idx]:
 				inventory[idx] = inventory[idx].duplicate()
 
-func index_inventory(Dictionary) -> void:       # This is the setter
+func index_inventory() -> void:       # This is the setter
 	inventory_by_category = {}
 	for item in inventory:
 		if item is EntityItem:
@@ -22,6 +35,6 @@ func index_inventory(Dictionary) -> void:       # This is the setter
 				inventory_by_category[item.category_type] = [item]
 				
 func get_inventory_by_category() -> Dictionary:    #  The Getter
-	if not inventory_by_category:
-		index_inventory(inventory_by_category)
+#	if not inventory_by_category:
+#		index_inventory(inventory_by_category)
 	return inventory_by_category
